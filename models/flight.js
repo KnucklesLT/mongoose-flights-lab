44218b30a0ccb7af4f7d5890e395f2fa0f3aace1
Luigi T <luigitejada@yahoo.com>
2022-09-24 21:31:06 -0400

    add new, edit, delete, and update functionality

---
+++ models/flight.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American','Southwest','United', 'Delta', 'Jet Blue'],
+  },
+
+  airport: {
+    type: String, 
+    enum: ['AUS','DFW','DEN','LAX','SAN', 'JFK', 'LGA'],
+    default: 'DEN',
+  },
+
+  flightNo: {
+    type: Number,
+    required: true,
+    min:10,
+    max:9999,
+  },
+
+  departs: {
+    type: Date,
+    default: function(){
+      return new Date().getFullYear()
+    }
+  }
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
---
+++ routes/flights.js
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import * as flightCtrl from '../controllers/flights.js'
+
+const router = Router()
+
+/* GET users listing. */
+router.get('/', flightCtrl.index)
+
+router.get('/new', flightCtrl.new)
+
+router.get('/:id', flightCtrl.show)
+
+router.get('/:id/edit', flightCtrl.edit)
+
+router.post('/', flightCtrl.create)
+
+router.delete('/:id', flightCtrl.delete)
+
+router.put('/:id', flightCtrl.update)
+
+export {
+  router
+}
